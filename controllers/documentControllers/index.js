b0119ece528e310d03d4a600bf5fc586b897fc84
RehanUlHassanAlvi <rehan_ulhassan@yahoo.com>
2024-06-06 16:30:56 +0500

    document index.js fix

--- controllers/documentControllers/index.js
+++ controllers/documentControllers/index.js
@@ -1,10 +1,10 @@
 
-const certificateOfIncorporationController = require('./certificateOfIncorporationController');
+const certificateOfIncorporationController = require('./CertificateOfIncorporationController');
 const identityDocumentController = require('./identityDocumentController');
-const proofOfIncomeController = require('./proofOfIncomeController');
-const proofOfOperatingAddressController = require('./proofOfOperatingAddressController');
-const proofOfResidenceController = require('./proofOfResidenceController');
-const taxClearanceController = require('./taxClearanceController');
+const proofOfIncomeController = require('./ProofOfIncomeController');
+const proofOfOperatingAddressController = require('./ProofOfOperatingAddressController');
+const proofOfResidenceController = require('./ProofOfResidenceController');
+const taxClearanceController = require('./TaxClearanceController');
 
 module.exports = {
   certificateOfIncorporationController,
