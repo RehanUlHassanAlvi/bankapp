c70b96b17770daec5a3b7dc1024da631d7ca59b5
PC <rehan_ulhassan@yahoo.com>
2024-07-19 18:17:07 +0500

    email scheduler added

--- models/Documents.js
+++ models/Documents.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
 const User = require('./Users'); // Adjust the path as needed
 const DocumentType = require('./DocumentTypes'); // Adjust the path as needed
-
+const RequestedDocument=require('./RequestedDocuments')
 const Document = sequelize.define('Documents', {
   id: {
     type: DataTypes.INTEGER,
