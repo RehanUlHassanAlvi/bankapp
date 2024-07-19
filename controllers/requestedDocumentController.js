17f19471e003137d150f60b8a8a73a7c893e26c4
PC <rehan_ulhassan@yahoo.com>
2024-07-19 16:36:11 +0500

    email scheduler added

--- controllers/requestedDocumentController.js
+++ controllers/requestedDocumentController.js
@@ -1,24 +1,41 @@
 const {createDocument}=require('../controllers/docController')
 const RequestedDocument=require('../models/RequestedDocuments')
-// Get a specific identity document by ID
+
 const requestDocument = async (req, res) => {
-    const businessId = req.user.id; // Get user ID from the authenticated token
-    const {userId,documentTypeId }=req.body
+  const businessId = req.user.id; // Get user ID from the authenticated token
+  const { userId, documentTypeId } = req.body;
 
   try {
-    let document=await createDocument(userId,documentTypeId,'requested');
-    let requestedDocument={
-        businessId,userId,
-        
-        documentId: document.id
+    // Check if a document with the specified documentTypeId already exists for the businessId
+    const existingRequest = await RequestedDocument.findOne({
+      include: [{
+        model: Document,
+        where: { documentTypeId: documentTypeId },
+        attributes: []
+      }],
+      where: { businessId: businessId }
+    });
+
+    if (existingRequest) {
+      return res.status(400).json({ message: 'Document already requested' });
     }
-    let reqDoc=await RequestedDocument.create(requestedDocument)
-    return res.status(200).json({ message: 'Document Requested Successfully',reqDoc });
-} catch (error) {
-  console.error('Error updating user status:', error);
-  return res.status(500).json({ message: 'Internal server error' });
-}
+
+    // Create the document if it does not already exist
+    let document = await createDocument(userId, documentTypeId, 'requested');
+    let requestedDocument = {
+      businessId,
+      userId,
+      documentId: document.id
+    };
+    let reqDoc = await RequestedDocument.create(requestedDocument);
+
+    return res.status(200).json({ message: 'Document Requested Successfully', reqDoc });
+  } catch (error) {
+    console.error('Error updating user status:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
-module.exports = {requestDocument}
+module.exports = { requestDocument };
+
   
