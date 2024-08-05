1e22beb06b7211eda11fa303804aae36466d99e4
RehanUlHassanAlvi <76509175+RehanUlHassanAlvi@users.noreply.github.com>
2024-08-05 12:52:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.use('/requestedDoc',reqDocumentRoute)
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
